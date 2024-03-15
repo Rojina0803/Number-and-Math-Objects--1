c2ccc93eda7f772a21bf083867866bb38015089e
Rojina Dhakal <128691590+Rojina0803@users.noreply.github.com>
2024-03-15 18:00:33 +0545

    Add files via upload

---
+++ 15mar.js
@@ -0,0 +1,78 @@
+//Numbers and Dates
+// let Num=0b1101
+// console.log(typeof(Num));
+// console.log(Num);
+
+// let num1=0x8967
+// let num2=0o345
+// console.log(num1+num2);
+
+// function multiply(x, y) {
+//     if (x * y > Number.MAX_VALUE) {
+//       return 'Process as Infinity';
+//     }
+//     return x * y;
+//   }
+//   console.log(multiply(8,9));
+
+//   function divide(p,q){
+//     if (p/q >Number.MIN_VALUE)
+//     {
+//         console.log("~~~~~~");
+//     }
+//     return p/q;
+//   }
+// console.log(divide(2,3));
+
+// const x = Number.MAX_SAFE_INTEGER + 1;
+// const y = Number.MAX_SAFE_INTEGER + 2;
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(x);
+// console.log(y);
+
+// const num3 = Number.MIN_SAFE_INTEGER - 1;
+// const num4 = Number.MIN_SAFE_INTEGER - 2;
+
+// console.log(Number.MIN_SAFE_INTEGER );
+// console.log("MIN_SAFE_INTEGER ::",x);
+// console.log("MIN_SAFE_INTEGER :", y);
+
+// function check(x){
+//     if(x==Number.NaN){
+//         return null;
+//     }
+// }
+// console.log(check(Number.NaN));
+
+// let n= 0
+// let bigNumber = Number.MAX_VALUE * 2;
+
+// if (bigNumber === Number.POSITIVE_INFINITY) {
+//   bigNumber = n;
+// }
+
+
+// const balance= new Number(100)
+// // console.log(typeof(balance));
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3));
+// console.log(typeof(balance));
+
+// // const otherNumber= 23.8966
+// const otherNumber= 123.8966
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds= 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+///Math Objects
+
+console.log(Math) 
+console.log(Math.abs(-6));
+console.log(Math.PI);
+console.log(Math.cos(90));
+console.log(Math.atan(90));
